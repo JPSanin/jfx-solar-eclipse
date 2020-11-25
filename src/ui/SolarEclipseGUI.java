163957080ee06111c83700362572d1e85735a3e1
Juan Esteban Caicedo <juancaicedo2000@gmail.com>
2020-11-25 10:22:29 -0500

    skeleton fxml

--- src/ui/SolarEclipseGUI.java
+++ src/ui/SolarEclipseGUI.java
@@ -1,5 +1,39 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
 public class SolarEclipseGUI {
+
+    @FXML
+    private Pane sky;
+
+    @FXML
+    private Circle moon;
+
+    @FXML
+    private Circle crater1;
+
+    @FXML
+    private Circle crater3;
+
+    @FXML
+    private Circle crater2;
+
+    @FXML
+    private Slider slider;
+
+    @FXML
+    public void moveMoon(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void stopMoon(ActionEvent event) {
+
+    }
 
 }
